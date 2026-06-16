1909431bb8cd959c9d130956dd272da3a48e35c2
jkellogg-git <jordan.a.kellogg@gmail.com>
2026-06-16 10:44:25 -0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,18 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        stackCopy = self.stack.copy()
+        stackCopy.sort(reverse = True)
+        return stackCopy[-1]
